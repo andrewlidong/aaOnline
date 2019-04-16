255ec58d3653d4536e2693bbc9172da779f1a81e
Andrew Li Dong <andrewdong1994@gmail.com>
2019-04-16 14:27:36 -0400

    BST Project Finished

--- bst_project/lib/bst.js
+++ bst_project/lib/bst.js
@@ -8,7 +8,64 @@ class TreeNode {
 
 
 class BST {
-   
+   constructor() {
+       this.root = null;
+   }
+
+   insert(val, root=this.root) {
+        if (!this.root) {
+            this.root = new TreeNode(val);
+            return;
+        }
+
+        if (val < root.val) {
+            if (!root.left) {
+                root.left = new TreeNode(val);
+            } else {
+                this.insert(val, root.left);
+            }
+        } else {
+            if (!root.right) {
+                root.right = new TreeNode(val);
+            } else {
+                this.insert(val, root.right);
+            }
+        }
+   }
+
+   searchRecur(val, root=this.root) {
+       if (!root) return false;
+
+       if (val < root.val) {
+            return this.searchRecur(val, root.left);
+       } else if (val > root.val) {
+            return this.searchRecur(val, root.right);
+       } else {
+           return true;
+       }
+   }
+
+   searchIter(val) {
+       let curr = this.root;
+       while (curr !== null) {
+           if (val < curr.val) {
+               curr = curr.left;
+           } else if (val > curr.val) {
+               curr = curr.right;
+           } else {
+               return true;
+           }
+       }
+       return false;
+   }
+
+   inOrderPrint(root = this.root) {
+       if (!root) return;
+
+       this.inOrderPrint(root.left);
+       console.log(root.val);
+       this.inOrderPrint(root.right);
+   }
 }
 
 module.exports = {
--- bst_project/lib/leet_code_110.js
+++ bst_project/lib/leet_code_110.js
@@ -2,6 +2,13 @@
 //  https://leetcode.com/problems/balanced-binary-tree/
 
 
-function isBalanced(root) {
+function getHeight(root) {
+    if (!root) return -1;
+    return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+}
 
+function isBalanced(root) {
+    if (!root) return true;
+    let heightDifference =  Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1;
+    return heightDifference && isBalanced(root.left) && isBalanced(root.right);
 }
