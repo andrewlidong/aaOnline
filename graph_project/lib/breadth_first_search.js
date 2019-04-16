3da982220c9c9cb94d4e313387f99c00d0e48995
Andrew Li Dong <andrewdong1994@gmail.com>
2019-04-16 17:27:32 -0400

    Finished graph project

--- graph_project/lib/breadth_first_search.js
+++ graph_project/lib/breadth_first_search.js
@@ -1,6 +1,28 @@
 function breadthFirstSearch(startingNode, targetVal) {
+    let queue = [ startingNode ];
+    let visited = new Set();
 
+    while (queue.length) {
+        let node = queue.shift();
+
+        if (visited.has(node)) continue;
+        visited.add(node);
+        
+        if (node.val === targetVal) return node;
+        queue.push(...node.neighbors);
+    }
+
+    return null;
 }
+
+// function depthFirst(startingNode, visited = new Set()) {
+//     if (visited.has(startingNode)) return;
+//     visited.add(startingNode);
+//     console.log(startingNode.val);
+//     startingNode.neighbors.forEach((neighbor) => depthFirst(neighbor));
+// }
+
+
 
 module.exports = {
     breadthFirstSearch
